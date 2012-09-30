1c061e1f7f9c92685478f80346dc6f5b6f0c9b1a
thatSignal <thevirtunuat@gmail.com>
2012-09-29 21:56:50 -0700

    adding stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '6dcf36f137e915d70122ce6ee780a2cb1e86ea0dd66b92496584f18fbcc722be787bf60965ea2cc2187a977c7d662b2b93027dfe65301d02a271bd84f0218aab'
